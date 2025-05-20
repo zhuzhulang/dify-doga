edf3e4d3cbab8f9c6ae96a2b457746d32e8eeb01
xx <xx@example.com>
2025-05-20 11:01:51 +0800

    初始化模板及代码

---
+++ llm/llm.py
@@ -0,0 +1,97 @@
+from core.model_runtime.entities.llm_entities import LLMResult, LLMResultChunk, LLMResultChunkDelta
+from core.model_runtime.model_providers.__base.large_language_model import LargeLanguageModel
+from core.model_runtime.errors.invoke import (
+    InvokeAuthorizationError,
+    InvokeBadRequestError,
+    InvokeConnectionError,
+    InvokeError,
+    InvokeRateLimitError,
+    InvokeServerUnavailableError,
+)
+
+class DogaError(Exception):
+    pass
+
+class DogaAPIError(DogaAPIError):
+    message: str
+    request: httpx.request
+    body: object | None
+
+    def __init__(self, message, request, *, body) -> None:  # noqa: ARG002
+        super().__init__(message)
+        self.request = request
+        self.message = message
+        self.body = body
+
+class DogaLargeLanguageModel(LargeLanguageModel):
+    def _invoke(self, model: str, credentials: dict,
+            prompt_messages: list[PromptMessage], model_parameters: dict,
+            tools: Optional[list[PromptMessageTool]] = None, stop: Optional[List[str]] = None,
+            stream: bool = True, user: Optional[str] = None) \
+        -> Union[LLMResult, Generator]:
+    """
+    Invoke large language model
+
+    :param model: model name
+    :param credentials: model credentials
+    :param prompt_messages: prompt messages
+    :param model_parameters: model parameters
+    :param tools: tools for tool calling
+    :param stop: stop words
+    :param stream: is stream response
+    :param user: unique user id
+    :return: full response or stream response chunk generator result
+    """
+    response = ["11111","22222","33333","44444"]
+    if stream:
+        return self._handle_stream_response(response)
+    return self._handle_sync_response(response)
+
+def get_num_tokens(self, model: str, credentials: dict, prompt_messages: list[PromptMessage],
+                   tools: Optional[list[PromptMessageTool]] = None) -> int:
+    """
+    Get number of tokens for given prompt messages
+
+    :param model: model name
+    :param credentials: model credentials
+    :param prompt_messages: prompt messages
+    :param tools: tools for tool calling
+    :return:
+    """
+    return 0
+
+def validate_credentials(self, model: str, credentials: dict) -> None:
+    """
+    Validate model credentials
+
+    :param model: model name
+    :param credentials: model credentials
+    :return:
+    """
+    pass
+
+@property
+def _invoke_error_mapping(self) -> dict[type[InvokeError], list[type[Exception]]]:
+    """
+    Map model invoke error to unified error
+    The key is the error type thrown to the caller
+    The value is the error type thrown by the model,
+    which needs to be converted into a unified error type for the caller.
+
+    :return: Invoke error mapping
+    """
+    return {
+        InvokeConnectionError: [DogaAPIError],
+        InvokeServerUnavailableError: [DogaAPIError],
+        InvokeRateLimitError: [DogaAPIError],
+        InvokeAuthorizationError: [DogaAPIError],
+        InvokeBadRequestError: [
+            DogaAPIError
+        ],
+    }
+
+def _handle_stream_response(self, response) -> Generator:
+    for chunk in response:
+          yield chunk
+def _handle_sync_response(self, response) -> LLMResult:
+    return LLMResult(**response)
